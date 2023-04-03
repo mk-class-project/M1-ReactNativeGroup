f2a90c62a380401e0fc35d5eee622a249084c081
Marksu-u <Marksu-u@users.noreply.github.com>
2023-04-03 23:09:30 +0200

    scrolled view on calendar

--- YourFitnessBuddy/src/components/calendar/index.js
+++ YourFitnessBuddy/src/components/calendar/index.js
@@ -1,31 +1,46 @@
 import React, { useState } from 'react';
-import {
-  CalendarContainer,
-  DayContainer,
-  CalendarDayBox,
-  DayBoxText,
-  HeaderText,
-  DataContainer,
-  DataText,
-} from './styles';
+import { ScrollView, Dimensions } from 'react-native';
+import { WeekContainer, Day, DayText, DayData, DaySeparator } from './styles';
 
 const Calendar = () => {
   const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const [selectedDay, setSelectedDay] = useState(null);
+  const [selectedDay, setSelectedDay] = useState(0);
+  const screenWidth = Dimensions.get('window').width;
+
+  const sampleData = {
+    Mon: 'Task A',
+    Tue: 'Task B',
+    Wed: 'Task C',
+    Thu: 'Task D',
+    Fri: 'Task E',
+    Sat: 'Task F',
+    Sun: 'Task G',
+  };
+
+  const handleScroll = (event) => {
+    const scrollX = event.nativeEvent.contentOffset.x;
+    const newIndex = Math.round(scrollX / screenWidth);
+    setSelectedDay(newIndex % daysOfWeek.length);
+  };
 
   return (
-      <CalendarContainer>
-        <DayContainer>
-          {daysOfWeek.map((day, index) => (
-            <CalendarDayBox key={day} isSelected={selectedDay === index} onPress={() => setSelectedDay(index)}>
-              <DayBoxText isSelected={selectedDay === index}>{day}</DayBoxText>
-              <DataText>No data for this day.</DataText>
-            </CalendarDayBox>
-          ))}
-        </DayContainer>
-      </CalendarContainer>
+    <WeekContainer>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={handleScroll}
+      >
+        {daysOfWeek.map((day, index) => (
+          <Day key={index} width={screenWidth}>
+            <DayText selected={index === selectedDay}>{day}</DayText>
+            <DaySeparator />
+            <DayData>{sampleData[day]}</DayData>
+          </Day>
+        ))}
+      </ScrollView>
+    </WeekContainer>
   );
 };
 
 export default Calendar;
-
--- YourFitnessBuddy/src/components/calendar/styles.js
+++ YourFitnessBuddy/src/components/calendar/styles.js
@@ -1,74 +1,30 @@
 import styled from 'styled-components/native';
 
-export const CalendarContainer = styled.View`
-  flex-direction: column;
-  width: 95%;
-  height: 50%;
+export const WeekContainer = styled.View`
+  flex: 1;
   background-color: #fff;
-  border-radius: 10px;
-  margin-bottom: 20px;
+`;
+
+export const Day = styled.View`
+  width: ${(props) => props.width}px;
+  flex: 1;
+  align-items: center;
   padding: 16px;
 `;
 
-export const CalendarWeek = styled.View`
-  flex-direction: row;
-  justify-content: space-around;
-  width: 100%;
-  margin-bottom: 16px;
-  border-bottom-width: 1px;
-  border-color: #CCCCCC;
-`;
-
-export const CalendarDay = styled.View`
-  align-items: center;
-  justify-content: center;
-  width: 40px;
-  height: 40px;
-  border-radius: 20px;
-`;
-
 export const DayText = styled.Text`
-  font-size: 14px;
-  font-weight: bold;
-  color: #999999;
-  text-align: left;
-`;
-
-export const DayContainer = styled.View`
-  flex-direction: row;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  width: 100%;
-  border-top-width: 1px;
-  border-color: #CCCCCC;
-`;
-
-export const CalendarDayBox = styled.TouchableOpacity`
-  align-items: center;
-  justify-content: center;
-  width: 40px;
-  height: 40px;
-  border-radius: 20px;
-  background-color: ${props => props.isSelected ? "#007FFF" : "white"};
-`;
-
-export const DayBoxText = styled.Text`
-  font-size: 16px;
-  font-weight: bold;
-  color: ${props => props.isSelected ? "white" : "black"};
-`;
-
-export const HeaderText = styled.Text`
   font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 16px;
+  color: ${(props) => (props.selected ? 'blue' : 'black')};
+  margin-bottom: 8px;
 `;
 
-export const DataContainer = styled.View`
-  margin-top: 32px;
+export const DaySeparator = styled.View`
+  width: 80%;
+  height: 1px;
+  background-color: #ccc;
+  margin-bottom: 8px;
 `;
 
-export const DataText = styled.Text`
+export const DayData = styled.Text`
   font-size: 16px;
-  color: #999999
 `;
