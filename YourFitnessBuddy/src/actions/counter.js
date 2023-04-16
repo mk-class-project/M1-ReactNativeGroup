3f7c3aa7cedef7d7a6d7d7cfc080d26747c73001
haigss <haig.semerdjian98@gmail.com>
2023-04-16 18:26:24 +0200

    counter

--- YourFitnessBuddy/src/actions/counter.js
+++ YourFitnessBuddy/src/actions/counter.js
@@ -3,3 +3,8 @@ export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 export const incrementCounter = () => ({
   type: INCREMENT_COUNTER,
 });
+
+export const updateCounter = (counter) => ({
+  type: 'UPDATE_COUNTER',
+  payload: counter,
+});
